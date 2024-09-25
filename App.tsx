aa5935ab71e91d06f5e6da7b064d461361b2cf93
tokismos <39965111+tokismos@users.noreply.github.com>
2024-09-25 01:05:20 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <StatusBar style="auto" />
+    </View>
+  );
+}
