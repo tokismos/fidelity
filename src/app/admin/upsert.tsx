8a2d9956fc73b9ce7164ec9b1cadbe8e8120f838
mohammed <medber1997@gmail.com>
2024-11-25 12:51:24 +0100

    Refactor submit reward to hook

--- src/app/admin/upsert.tsx
+++ src/app/admin/upsert.tsx
@@ -1,23 +1,18 @@
 import { ButtonWithIndicator } from "@/components/ButtonWithIndicator"
-import { CapturePhoto } from "@/components/CapturePhoto"
 import { RewardConfigFields } from "@/components/RewardConfigFields"
 import { FormField } from "@/components/RewardFormField"
-import { useAddReward } from "@/hooks/useAddReward"
 import { useGetRewardById } from "@/hooks/useGetRewardById"
-import { useUpdateReward } from "@/hooks/useUpdateReward"
+import { useRewardSubmit } from "@/hooks/useRewardSubmit"
 import { Reward, REWARD_TYPES } from "@/types"
-import { uploadImageToBucket } from "@/utils/uploadImageToBucket"
 import { useLocalSearchParams } from "expo-router"
 import { useEffect, useState } from "react"
-import { View, Text, ScrollView, TextInput, TextInputProps } from "react-native"
+import { View, Text, ScrollView } from "react-native"
 
 type FormDataProps<T extends REWARD_TYPES> = Omit<Reward<T>, "storeId" | "status"> & { image: string | null }
 
 export default function UpsertRewardScreen() {
   const { type, rewardId } = useLocalSearchParams<{ type: REWARD_TYPES; rewardId: string }>()
-  const { rewardById, isLoading, error } = useGetRewardById({ rewardId })
-
-  const isEditMode = Boolean(rewardId)
+  const { rewardById, error } = useGetRewardById({ rewardId })
 
   const [formData, setFormData] = useState<FormDataProps<typeof type>>({
     title: "",
@@ -27,8 +22,7 @@ export default function UpsertRewardScreen() {
     image: null,
   })
 
-  const { addReward, isPending } = useAddReward()
-  const { updateReward, isPending: updateIsPending } = useUpdateReward({ rewardId })
+  const { submitReward, isLoading, isEditMode } = useRewardSubmit({ rewardId })
 
   useEffect(() => {
     if (rewardById) {
@@ -36,24 +30,6 @@ export default function UpsertRewardScreen() {
     }
   }, [rewardById])
 
-  const handleSubmit = async () => {
-    try {
-      const reward = { ...formData }
-
-      if (formData.image) {
-        ;(reward.config as unknown as any).image_path = await uploadImageToBucket(formData.image)
-      }
-
-      if (isEditMode) {
-        updateReward({ updatedReward: { ...reward } })
-        return
-      }
-
-      addReward({ ...reward })
-    } catch (e) {
-      console.log("there is an error ", e)
-    }
-  }
   return (
     <ScrollView className="flex-1 bg-gray-50 p-4">
       <Text className="mb-6 text-2xl font-bold">Create New Reward</Text>
@@ -61,8 +37,7 @@ export default function UpsertRewardScreen() {
       <View className="rounded-lg bg-white p-4 shadow-sm">
         <FormField
           label="Reward Name"
-          // defaultValue="BUY N GET 1"
-          value={formData.title ?? "BUY N GET 1"}
+          value={formData.title}
           onChangeText={(title) => setFormData((prev) => ({ ...prev, title }))}
         />
 
@@ -77,8 +52,8 @@ export default function UpsertRewardScreen() {
 
         <ButtonWithIndicator
           title={isEditMode ? "Update reward" : "Create reward"}
-          isLoading={isPending || updateIsPending}
-          onPress={handleSubmit}
+          isLoading={isLoading}
+          onPress={() => submitReward(formData)}
         />
       </View>
     </ScrollView>
--- src/utils/getImageFromBucket.ts
+++ src/utils/getImageFromBucket.ts
@@ -1,7 +1,7 @@
 import React from "react"
 import { supabase } from "./supabase"
 
-export const getImageFromBucket = async (path) => {
+export const getImageFromBucket = async (path: string) => {
   const { data, error } = await supabase.storage.from("images").createSignedUrl(path, 3600)
 
   return data
